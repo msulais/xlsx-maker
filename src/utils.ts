ab1c3529f9625eabd28e6ad5208d592d9dd8c103
Muhammad Sulais <muh.sulais@gmail.com>
2025-10-31 22:01:10 +0700

    add util

--- src/utils.ts
+++ src/utils.ts
@@ -15,6 +15,23 @@ export function lettersToNumber(letters: string): number {
     return result
 }
 
+export function numberToLetters(num: number): string {
+    if (num < 1 || !Number.isInteger(num)) {
+        throw new Error('Input must be a positive integer')
+    }
+
+    let result = ''
+    while (num > 0) {
+        num--
+        const remainder = num % 26
+        const letter = String.fromCharCode(65 + remainder) // 65 = 'A'
+        result = letter + result
+        num = Math.floor(num / 26)
+    }
+
+    return result
+}
+
 export function dateDiffInDays(date1: Date, date2: Date): number {
 	const MS_PER_DAY = 1000 * 60 * 60 * 24
 	const utc1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate())
